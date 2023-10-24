68e477977406c75a4b74a63ec27ab9c7b48adab9
Tizz1e <kaluginaleksey133@yandex.ru>
2023-10-24 23:22:21 +0300

    god, please, excuse my sins

---
+++ src/main/java/edu/hw1/Task1.java
@@ -0,0 +1,26 @@
+package edu.hw1;
+
+public class Task1 {
+    final static int SECONDS_PER_MINUTE = 60;
+
+    private Task1() {
+
+    }
+
+    public static int minutesToSeconds(String time) throws IllegalArgumentException {
+        String[] splitTime = time.split(":");
+        if (splitTime.length != 2) {
+            return -1;
+        }
+        try {
+            int minutes = Integer.parseInt(splitTime[0]);
+            int seconds = Integer.parseInt(splitTime[1]);
+            if (minutes < 0 || seconds < 0 || seconds >= SECONDS_PER_MINUTE) {
+                return -1;
+            }
+            return SECONDS_PER_MINUTE * minutes + seconds;
+        } catch (NumberFormatException e) {
+            return -1;
+        }
+    }
+}
